217219f00391151d28141fcfbeca954121dea11b
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-01-18 22:55:23 -0300

    fix: nao vai

--- components/QuadroPost.tsx
+++ components/QuadroPost.tsx
@@ -5,33 +5,35 @@ interface QuadroPostProps {}
 
 export default function QuadroPost(props: QuadroPostProps) {
   return (
-    <div className="mt-2">
-      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        <Image
-          src={FotoPadreRamon1}
-          alt=""
-          width={384}
-          height={512}
-          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-        />
-        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-          <blockquote>
-            <p className="text-lg font-medium">
-              “Padre Ramon assumiu uma missão não apenas de evangelização, mas
-              de dedicação aqueles que precisavam de acolhimento, carinho e
-              conforto. Ele teve um papel importante na consolidação da
-              comunidade neocatecumenal em Franca e região e, por meio dele,
-              modificou, para melhor, a vida de milhares de pessoas”,
-            </p>
-          </blockquote>
-          <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text-sky-400">Deputado Roberto Engler</div>
-            <div className="text-slate-700 dark:text-slate-500">
-              Franca - SP, Brasil
-            </div>
-          </figcaption>
-        </div>
-      </figure>
+    <div className="mt-2 grid-flow-dense grid-cols-2">
+      <div className="border-solid border-8 bg-green-400">
+        <p>
+          Padre Ramón Sevillano Valencia nasceu em Cirauqui (Navarra), na
+          Espanha, em 1943. Realizou toda a sua formação religiosa, com estudos
+          em Filosofia e Teologia naquele país. Já como sacerdote, exerceu o seu
+          ministério como educador em escolas salesianas de Barcelona, em
+          bairros e distritos. Em 1979, pediu aos superiores salesianos para se
+          dedicar plenamente ao apostolado como itinerante do Caminho
+          Neocatecumenal. Exerceu a missão itinerante nas regiões de La Mancha,
+          Castilla-León, Astúrias, na Espanha, e também em Portugal, partindo
+          para o Brasil no ano de 1990. Em terras brasileiras, trabalhou em
+          diversas dioceses sempre como Presbítero Itinerante: Franca, Brasília,
+          São Paulo e Jundiaí.
+        </p>
+        <p>
+          Ajudou a fundar o Seminário Missionário Arquidiocesano “Redemptoris
+          Mater” de Brasília, um dos mais florescentes do Brasil. Profundamente
+          unido com a sua comunidade neocatecumenal de origem, Padre Ramón
+          Sevillano passou os últimos anos de sua vida sem ter casa nem tempo
+          para si mesmo, sendo um a mais nas famílias que o acolhiam, sempre
+          disposto a partir para outros lugares, contatar pessoas para pregar a
+          salvação, o amor de Deus e sua predileção pelos pecadores.
+        </p>
+      </div>
+
+      <div className="bg-yellow-300 ">
+        <Image src={FotoPadreRamon1} alt="Foto do padre Ramon"></Image>
+      </div>
     </div>
   );
 }
