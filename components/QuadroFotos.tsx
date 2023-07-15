2510a78f4e32092cede0a7ffbbcab86c37fe3991
Leonardo Cintra <leonardo.ncintra@outlook.com>
2023-07-15 14:32:44 -0300

    fix <img>

--- components/QuadroFotos.tsx
+++ components/QuadroFotos.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import CardFoto from "./CardFoto";
 import ButtonLaranja from "./Button";
 
 interface QuadroFotosProps {}
@@ -61,21 +60,27 @@ export default function QuadroFotos(props: QuadroFotosProps) {
           <div className="flex flex-wrap md:-m-2 -m-1">
             <div className="flex flex-wrap w-1/2">
               <div className="md:p-2 p-1 w-1/2">
-                <img
+                <Image
+                  width={200}
+                  height={200}
                   alt="gallery"
                   className="w-full object-cover h-full object-center block"
                   src="https://dummyimage.com/500x300"
                 />
               </div>
               <div className="md:p-2 p-1 w-1/2">
-                <img
+                <Image
+                  width={200}
+                  height={200}
                   alt="gallery"
                   className="w-full object-cover h-full object-center block"
                   src="https://dummyimage.com/501x301"
                 />
               </div>
               <div className="md:p-2 p-1 w-full">
-                <img
+                <Image
+                  width={200}
+                  height={200}
                   alt="gallery"
                   className="w-full h-full object-cover object-center block"
                   src="https://dummyimage.com/600x360"
@@ -84,21 +89,27 @@ export default function QuadroFotos(props: QuadroFotosProps) {
             </div>
             <div className="flex flex-wrap w-1/2">
               <div className="md:p-2 p-1 w-full">
-                <img
+                <Image
+                  width={200}
+                  height={200}
                   alt="gallery"
                   className="w-full h-full object-cover object-center block"
                   src="https://dummyimage.com/601x361"
                 />
               </div>
               <div className="md:p-2 p-1 w-1/2">
-                <img
+                <Image
+                  width={200}
+                  height={200}
                   alt="gallery"
                   className="w-full object-cover h-full object-center block"
                   src="https://dummyimage.com/502x302"
                 />
               </div>
               <div className="md:p-2 p-1 w-1/2">
-                <img
+                <Image
+                  width={200}
+                  height={200}
                   alt="gallery"
                   className="w-full object-cover h-full object-center block"
                   src="https://dummyimage.com/503x303"
