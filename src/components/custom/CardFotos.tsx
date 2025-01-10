f378add13a8dd5072b678c40d498d8c39e1fb464
Leonardo Cintra <leonardo.ncintra@outlook.com>
2025-01-09 22:23:12 -0300

    :bug: fix index map fotos

--- src/components/custom/CardFotos.tsx
+++ src/components/custom/CardFotos.tsx
@@ -6,8 +6,9 @@ export default function CardFotos() {
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-20 mx-auto">
         <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-          {fotosData.map((f) => (
+          {fotosData.map((f, index) => (
             <CardFoto
+              key={index}
               titulo={f.titulo}
               descricao={f.subTitulo}
               imageUrl={f.imageUrl}
