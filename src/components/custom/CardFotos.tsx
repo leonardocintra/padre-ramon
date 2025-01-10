e5ec5caf6c031a85def86c806527e5f92a557a4b
Leonardo Cintra <leonardo.ncintra@outlook.com>
2025-01-09 22:21:44 -0300

    :zap: melhoria fotos

--- src/components/custom/CardFotos.tsx
+++ src/components/custom/CardFotos.tsx
@@ -1,3 +1,4 @@
+import { fotosData } from "@/data/fotos";
 import CardFoto from "./CardFoto";
 
 export default function CardFotos() {
@@ -5,36 +6,13 @@ export default function CardFotos() {
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-20 mx-auto">
         <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-          <CardFoto
-            titulo="Aspirantado Salesiano de Girona (Espanha)"
-            descricao="Tendo sentido o chamado a ser presbítero e missionário, ingressou, num primeiro momento, no Aspirantado Salesiano de Girona (Espanha) em 1957."
-            imageUrl="/img/card/salesianos.jpg"
-          />
-          <CardFoto
-            titulo="Redemptoris Matter de Brasília"
-            descricao="Padre Ramon com iniciadores do caminho Kiko Argüello (biblia) Carmen Hernandez no terreno que é  hoje o Seminário Missionário Redemptoris Matter de Brasilia. Clique na foto para conhecer a história do Seminário"
-            imageUrl="/img/brasilia/4.jpg"
-          />
-          <CardFoto
-            titulo="Padre Ramon em Brasília"
-            descricao="Padre Ramon acompanhando os iniciadores do Caminho Neocatecumenal em visita a Brasilia"
-            imageUrl="/img/brasilia/1.jpg"
-          />
-          <CardFoto
-            titulo="São João Paulo II"
-            descricao="Padre Ramon com o Santo Papa João Paulo II"
-            imageUrl="/img/canva2/joaopaulo.jpg"
-          />{" "}
-          <CardFoto
-            titulo="Trabalho voluntário"
-            descricao="Também fazia todos os trabalhos voluntários com alegria e dedicação"
-            imageUrl="/img/canva2/porco.jpg"
-          />{" "}
-          <CardFoto
-            titulo="Convivencias"
-            descricao="Participou de grandes convivencia com Kiko Argüello"
-            imageUrl="/img/canva2/kiko.jpg"
-          />
+          {fotosData.map((f) => (
+            <CardFoto
+              titulo={f.titulo}
+              descricao={f.subTitulo}
+              imageUrl={f.imageUrl}
+            />
+          ))}
         </div>
       </div>
     </section>
---
+++ src/data/fotos.ts
@@ -0,0 +1,129 @@
+export const fotosData = [
+  {
+    titulo: "Aspirantado Salesiano de Girona (Espanha)",
+    subTitulo: "Tendo sentido o chamado a ser presbítero e missionário, ingressou, num primeiro momento, no Aspirantado Salesiano de Girona (Espanha) em 1957.",
+    imageUrl: "/img/card/salesianos.jpg"
+  },
+  {
+    titulo: "Redemptoris Matter de Brasília",
+    subTitulo: "Padre Ramon com iniciadores do caminho Kiko Argüello (biblia) Carmen Hernandez no terreno que é  hoje o Seminário Missionário Redemptoris Matter de Brasilia. Clique na foto para conhecer a história do Seminário",
+    imageUrl: "/img/brasilia/4.jpg",
+  },
+  {
+    titulo: "Padre Ramon em Brasília",
+    subTitulo: "Padre Ramon acompanhando os iniciadores do Caminho Neocatecumenal em visita a Brasilia",
+    imageUrl: "/img/brasilia/1.jpg",
+  },
+  {
+    titulo: "São João Paulo II",
+    subTitulo: "Padre Ramon com o Santo Papa João Paulo II",
+    imageUrl: "/img/canva2/joaopaulo.jpg",
+  },
+  {
+    titulo: "Trabalho voluntário",
+    subTitulo: "Também fazia todos os trabalhos voluntários com alegria e dedicação",
+    imageUrl: "/img/canva2/porco.jpg",
+  },
+
+  {
+    titulo: "Convivencias",
+    subTitulo: "Participou de grandes convivencia com Kiko Argüello",
+    imageUrl: "/img/canva2/kiko.jpg",
+  },
+
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva2/1.jpg",
+  },
+
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva2/2.jpg",
+  },
+
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva2/3.jpg",
+  },
+
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva2/5.jpg",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva2/6.jpg",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva2/7.jpg",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva2/8.jpg",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva2/11.jpg",
+  },
+
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva2/12.jpg",
+  },
+
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva2/13.jpg",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva/1.png",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva/3.png",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva/6.png",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva/7.png",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva/8.png",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva/9.png",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva/10.png",
+  },
+  {
+    titulo: "REMOVER ?",
+    subTitulo: "Atenção! Remover foto ou colocar uma descrição melhor!",
+    imageUrl: "/img/canva/12.png",
+  },
+]
