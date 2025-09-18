907a4017ee3e9a53d9d7082638278a8c9ff9b173
leonardolnc <leonardolnc@gmail.com>
2025-09-18 22:45:35 +0000

    add cnpj

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,10 @@ export default function Footer() {
   return (
     <footer className="bg-[#0A3274] text-white text-center py-4">
       <p className="text-sm">
-        &copy; {new Date().getFullYear()} - CAMINHO NEOCATECUMENAL DO BRASIL
+        &copy; {new Date().getFullYear()} - Associacao Virgem do Terceiro Milenio para a Evangelizacao Itinerante
+      </p>
+      <p className="text-sm mt-2 mb-4">
+        CNPJ: 06.108.869/0001-30
       </p>
       <div className="text-xs mt-2">
         <Link href="/privacity" className="hover:underline">
